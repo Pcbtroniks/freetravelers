d77bc09982d44ebf5348227cfcec1efb18660e3f
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2022-09-12 16:25:02 -0500

    Added Breeze for Auth

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,9 @@ Route::get('/home', function () {
     return view('admin.home');
 
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/views/front/**/*.blade.php",
-    "./resources/**/views/layout/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "black-soft": "#71716A"
-      }
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+
+    plugins: [require('@tailwindcss/forms')],
+};
